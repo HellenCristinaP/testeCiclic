e1da0c8d6919850318499d3f6cc218261f7277a0
HellenCristina <hellencrissaraujo28@gmail.com>
2025-09-06 23:11:36 -0300

    Estrututa do site e um pouco de SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import ciclicLogo from './assets/ciclic-logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header><img src={ciclicLogo} alt="Ciclic Logo" /></header>
+      <form action="POST">
+        <label htmlFor="name">Nome</label>
+        <input type="text" id="name" placeholder="Digite seu nome" autoFocus />
+        <label htmlFor="monthlyFee">Mensalidade</label>
+        <input type="text" inputMode='numeric' id="monthlyFee" placeholder="100,00 - 9000,00" pattern="[0-9]{3,4},[0-9]{2}" required/>
+        <label htmlFor="timeInYears">Tempo (anos)</label>
+        <select name="timeInYears" id="timeInYears">
+          <option value="1">1 ano</option>
+          <option value="2">2 anos</option>
+          <option value="3">3 anos</option>
+        </select>
+        <input type="submit" value="Calcular" />
+      </form>
     </>
   )
 }
